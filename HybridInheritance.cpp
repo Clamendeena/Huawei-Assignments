005122ff3124e0ee96021b9bcc88993f172dfc5d
unknown <clamendeena@gmail.com>
2022-03-09 11:35:14 +0530

    Initial Commit

---
+++ HybridInheritance.cpp
@@ -0,0 +1,44 @@
+/*
+Hybrid Inheritance
+Combining various types of inheritance like multiple, simple, and hierarchical inheritance is known as hybrid inheritance
+*/
+
+#include <iostream>  
+using namespace std;  
+class vehicle  
+{  
+public:  
+vehicle()  
+    {  
+cout<< "This is a vehicle\n";  
+    }  
+};  
+class Car: public vehicle  
+{  
+public:  
+Car()  
+    {  
+cout<< "This is a car\n";  
+    }  
+};  
+class Racing  
+{  
+public:  
+Racing()  
+    {  
+cout<< "This is for Racing\n";  
+    }  
+};  
+class Ferrari: public Car, public Racing  
+{  
+public:  
+Ferrari()  
+    {  
+cout<< "Ferrari is a Racing Car\n";  
+    }  
+  
+};  
+int main() {  
+    Ferrari f;  
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Huawei Internship Assignments
+This Repository consists of Assignments
+
